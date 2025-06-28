c00b6c7c3998a0512c0e3cb98c704faf5e9a77b2
edvv1n <edvvinudofia@gmail.com>
2025-06-28 12:26:23 +0100

    my first movie app

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../css/NavBar.css'
+
+function NavBar() {
+  return (
+    <nav className='navbar'>
+        <div className='navbar-brand'>
+            <Link to="/">The Watch Hub</Link>
+        </div>
+        <div className='navbar-links'>
+            <Link to ="/" className='nav-link'>All Movies</Link>
+            <Link to ="/favorites" className='nav-link'>My Favorite Movies</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "f9d85f849f081d6c65dacadbbab7374a";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results;
+};
